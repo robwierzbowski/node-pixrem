d59ba46e7444bfd6b01b221cded5b254f0228bea
Rob Wierzbowski <hello@robwierzbowski.com>
2013-12-14 22:26:37 -0500

    Add Jasmine unit tests

--- test/test.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-var fs = require('fs');
-var pixrem = require('../lib/pixrem');
-var css = fs.readFileSync('./test/app/test.css', 'utf8');
-
-console.log(pixrem(css));
-console.log(pixrem(css, '200%'));
-console.log(pixrem(css, '200%', { replace: true }));
